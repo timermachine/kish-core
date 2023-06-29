f4021c559d309254d913fa95189fa33c5ca039f1
Henry Kemp <henry@ticaboo.co.uk>
2023-06-29 19:19:33 +0100

    "oitnoopit!"

--- src/lib/kit.sh
+++ src/lib/kit.sh
@@ -1,86 +1,82 @@
 #!/usr/bin/env bash
 
- 
 #  call this kit, move up a dir. add to geneated help.
 #   calling kit runs all .test files.
 #   parse file, search for onlyFname's, run only these tests. xeq
 #   call this kit, move up a dir. add to geneated help.
 
-
- source "$HOME/.kish/lib/colors.sh"
- source "$HOME/.kish/lib/util.sh" # moved kstate here - do we need utils?
+source "$HOME/.kish/lib/colors.sh"
+source "$HOME/.kish/lib/util.sh" # moved kstate here - do we need utils?
 
 # States for global tracking :
 #  kstate - test fail/pass counter
 #  xstate - disable/enable test runs
 #  TODO CHORE: refactor kstate and xstate to just state
- kstate_path="$HOME/.kish/temp/"
- TP="testpass" #filename to log test passes
- TF="testfail" #filename to log test fails
- TS="tesskip" #filename to log tesk skips (x/xoff)
- xstate_path="$HOME/.kish/temp/"
- XSTATE_FILE="xstate" #filename to log xstate (on/off) for test run exclusion
+kstate_path="$HOME/.kish/temp/"
+TP="testpass" #filename to log test passes
+TF="testfail" #filename to log test fails
+TS="tesskip"  #filename to log tesk skips (x/xoff)
+xstate_path="$HOME/.kish/temp/"
+XSTATE_FILE="xstate" #filename to log xstate (on/off) for test run exclusion
 
 # kstate_init id  sets to zero
-function kstate_init () {
-#   echo    "${YELLOW} kstate_init: $kstate_path$1"
-  echo 0 > "$kstate_path$1"
+function kstate_init() {
+  #   echo    "${YELLOW} kstate_init: $kstate_path$1"
+  echo 0 >"$kstate_path$1"
 }
 
 # increment counter
-function kstate_increment () {
-    let kincrement=$(cat  "$kstate_path$1")+1
-    echo $kincrement > "$kstate_path$1"
+function kstate_increment() {
+  let kincrement=$(cat "$kstate_path$1")+1
+  echo $kincrement >"$kstate_path$1"
 }
 
 # get id
-function kstate_get () {
-    echo $(cat "$kstate_path$1")
+function kstate_get() {
+  echo $(cat "$kstate_path$1")
 }
 
 # set id value
-function kstate_set () {
-    echo $1 > "$kstate_path$1"
+function kstate_set() {
+  echo $1 >"$kstate_path$1"
 }
-
 
 # ------- xstate block start --------
 
 #xreset id  sets to zero name:reset/init?
-function xstate_init () {
-  echo 0 > "$xstate_path$XSTATE_FILE"
+function xstate_init() {
+  echo 0 >"$xstate_path$XSTATE_FILE"
 }
 
 # get xstate
-function xstate_get () {
-    echo $(cat "$xstate_path$XSTATE_FILE")
+function xstate_get() {
+  echo $(cat "$xstate_path$XSTATE_FILE")
 }
 
 # set xstate value
-function xstate_set () {
-    echo $1 > "$xstate_path$XSTATE_FILE"
+function xstate_set() {
+  echo $1 >"$xstate_path$XSTATE_FILE"
 }
 # -------xstate block end--------
 
+kstate_init $TP
+kstate_init $TF
+kstate_init $TS
+xstate_init
 
- kstate_init $TP
- kstate_init $TF
- kstate_init $TS
- xstate_init
-
- # turns off tests - eg: eq, desc  
- function xon () {
+# turns off tests - eg: eq, desc
+function xon() {
   xstate_set 1
- }
+}
 # editors can upper case just x annoyingly
 # function X () {
 #   xstate_set 1
 #  }
 
- # turns back on (xstate=false) for eq, desc (if x previously called)
- function xoff () {
+# turns back on (xstate=false) for eq, desc (if x previously called)
+function xoff() {
   xstate_set 0
- }
+}
 
 function xeq() {
   # noop
@@ -88,55 +84,65 @@ function xeq() {
   return 0 # ok
 }
 
-function it () {
-   if [[ $(xstate_get) -eq 0 ]]; then
+# only feature: if any oit in test source file switch to it to oit.
+function it() {
+  _it_enabled "$1"
+}
+
+function oit() {
+  _it_enabled "$1"
+}
+function _it_disabled() {
+  return 0
+}
+
+function _it_enabled() {
+  if [[ $(xstate_get) -eq 0 ]]; then
     eval "$1"
-    else 
-      kstate_increment "$TS"
-   fi
-}
-
-function xit () {
+  else
     kstate_increment "$TS"
+  fi
 }
 
-function eq () {
+function xit() {
+  kstate_increment "$TS"
+}
+
+function eq() {
   # xs=$(xstate_get)
   # log_info "eq xstate: $xs"
-    # if [[ "$xs" -eq 0 ]]; then 
-    local testdesc="$1"
-    local result="$2"
-    local expected="$3"
+  # if [[ "$xs" -eq 0 ]]; then
+  local testdesc="$1"
+  local result="$2"
+  local expected="$3"
 
-    echo "  $testdesc:"
-    if [ "$result" == "$expected" ];then
-      kstate_increment "$TP"
-      printf "${IGre}"
-        echo "   √  $result."
-        echo "   == $expected."
-    else
-        kstate_increment "$TF"
-        printf "${IRed}"
-        echo "   x  $result."
-        echo "   != $expected."
-      fi
-      # reset color
-      printf "${IWhi}\n"
-    # fi
+  echo "  $testdesc:"
+  if [ "$result" == "$expected" ]; then
+    kstate_increment "$TP"
+    printf "${IGre}"
+    echo "   √  $result."
+    echo "   == $expected."
+  else
+    kstate_increment "$TF"
+    printf "${IRed}"
+    echo "   x  $result."
+    echo "   != $expected."
+  fi
+  # reset color
+  printf "${IWhi}\n"
+  # fi
 }
 
-
-function desc () {
+function desc() {
   [[ $(xstate_get) -eq 1 ]] && return 0
   # echo "------------------------ DESC -----------------------------------------------"
   #  printf ">>>>>>>>>>>> ${BIWhi} % ${IWhi} >>>>>>>>>>>> \n" "$1"
 
-   printf "${BIWhi}"
-   echo "$1"
+  printf "${BIWhi}"
+  echo "$1"
   printf "${IWhi}"
   echo ""
 }
-
 
 # cd's to aliases (++ check current dir has any .test files first, if so run from there instead, fallback to configured dir.)
 # search all .test files (++and sub dirs) ()
@@ -153,47 +159,59 @@ param : glob (multiple test files x*)
 '
 
 test_files=''
-if (( $# > 1 )); then
-   test_files="$@"
-else 
-    if [ -f "$1" ];then
-        test_files="$1" 
+if (($# > 1)); then
+  test_files="$@"
+else
+  if [ -f "$1" ]; then
+    test_files="$1"
+  else
+    #if $1 dir
+    if [ -d "$1" ]; then
+      test_files="$1/*.test*"
     else
-       #if $1 dir   
-       if [ -d "$1" ]; then
-         test_files="$1/*.test*"
-       else
-            test_files="$PWD/*.test*"
-        fi
+      test_files="$PWD/*.test*"
     fi
+  fi
 fi
 
 echo "test_files: $test_files"
 
-   for f in $test_files; do
-   echo ""
-   echo "-------------------------------- Test File: $f --------------------------------"
-   #  () subshell: to improve tests isolation.  kstate to track passes/failed tests
-   (
+for f in $test_files; do
+
+  source_code=$(cat "$f")
+
+  if [[ "$source_code" =~ .*oit*. ]]; then
+    # echo 'noopit!'
+    function it() {
+      _it_disabled
+    }
+  fi
+
+  echo ""
+  echo "-------------------------------- Test File: $f --------------------------------"
+  #  () subshell: to improve tests isolation.  kstate to track passes/failed tests
+  (
     source $f
-   )
-   echo ""
-   done
+  )
+  echo ""
+  # re-enable it for next test file.
+  # todo - dry code. reference base it. fucking mutation of functions grrr - ok as long as code under 200 lines ;)
+  function it() {
+    _it_enabled "$1"
+  }
+done
 
 testpasses=$(kstate_get "$TP")
 testfails=$(kstate_get "$TF")
 testskips=$(kstate_get "$TS")
-(( testcounter=testpasses+testfails ))
+((testcounter = testpasses + testfails))
 
- echo "##################### Tests Done ######################"
- echo ""
- echo  "${GREEN}$testpasses passed. ${RED} $testfails  failed. ${NORMAL} $testskips skipped. $testcounter tests ran."
+echo "##################### Tests Done ######################"
+echo ""
+echo "${GREEN}$testpasses passed. ${RED} $testfails  failed. ${NORMAL} $testskips skipped. $testcounter tests ran."
 
+xoff #turn x off for next test run.
 
- xoff #turn x off for next test run.
-
- # CI: 0: no errors; >0 erros. todo: TAP compliance.
+# CI: 0: no errors; >0 erros. todo: TAP compliance.
 # to figure out: return: can only `return' from a function or sourced script
 # return  "$testfails"
-
-
